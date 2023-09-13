fb0e854bd65abf55b1aa6c3eb53b349fbf0fc3cd
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2023-09-13 20:29:25 +0300

    added React Navigation

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoryGridTile from './components/CategoryGridTile';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
-    <StatusBar style='light'/>
     <View style={styles.container}>
-      <CategoriesScreen/>
-    </View>
+      <StatusBar style='auto'/>
+      <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+          </Stack.Navigator>
+      </NavigationContainer>
+      </View>
     </>
   );
 }
