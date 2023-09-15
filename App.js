0a65eb963903860cf9633e6a6830f9cebcb39766
Ahmed Jabareen <ahmed.jabareen1@gmail.com>
2023-09-15 22:27:20 +0300

    added drawer

--- App.js
+++ App.js
@@ -7,9 +7,30 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavouritesContextProvider from './store/context/fsvourite-context';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavouritesScreen from './screens/FavouritesScreen';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+    return (
+      <Drawer.Navigator screenOptions={
+        {
+          headerStyle: {
+          backgroundColor:'#351401' },
+          headerTintColor:'white', 
+          sceneContainerStyle:{backgroundColor:'#352f25'}
+        }
+      }> 
+          <Drawer.Screen name="Categories" component={CategoriesScreen} />
+          <Drawer.Screen name="Favourites Screen" component={FavouritesScreen} />
+      </Drawer.Navigator>
+
+    );
+}
+
 export default function App() {
   return (
     <>
@@ -17,7 +38,7 @@ export default function App() {
       <StatusBar style='light'/>
       <FavouritesContextProvider>
       <NavigationContainer>
-          <Stack.Navigator initialRouteName='MealsCategories' screenOptions={
+          <Stack.Navigator initialRouteName='Drawer' screenOptions={
             {
               headerStyle: {
                 backgroundColor:'#351401' },
@@ -25,9 +46,10 @@ export default function App() {
                 contentStyle:{backgroundColor:'#352f25'}
             }
           }>
-            <Stack.Screen name='MealsCategories' component={CategoriesScreen}
+            <Stack.Screen name='Drawer' component={DrawerNavigator}
             options={
-              {title: 'All Categories', 
+              {
+              headerShown: false
             }
             }/>
             <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} options={({route, navigation})=> {
